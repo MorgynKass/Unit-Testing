5e6a226d40f302d511fc89c3fe789e8a86033302
MorgynKass <morgynkass@gmail.com>
2024-04-23 12:33:15 -0600

    finished mocking-spies

---
+++ 01-starting-project/src/util/io.test.js
@@ -0,0 +1,24 @@
+import { test, expect, vi } from "vitest";
+import { promises as fs } from "fs";
+
+import { writeData } from "./io";
+
+vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
+
+test("should execute the writeFile method", () => {
+  const testData = "Test";
+  const testFilename = "test.txt";
+
+  writeData(testData, testFilename);
+
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData)
+});
